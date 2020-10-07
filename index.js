53748ad557d579b6441978ac1294baf909a11178
ym-project <ym-project@protonmail.com>
2020-10-07 17:32:48 +0800

    convert spaces to tabs

--- index.js
+++ index.js
@@ -6,49 +6,49 @@ const Vinyl = require('vinyl')
 const PLUGIN_NAME = 'gulp-esbuild'
 
 module.exports = function(options = {}) {
-    const entries = []
+	const entries = []
 
-    return new Transform({
-        objectMode: true,
-        transform(file, _, cb) {
-            if (!file.isBuffer()) {
-                return cb(new PluginError(PLUGIN_NAME, new TypeError('file should be a buffer')))
-            }
+	return new Transform({
+		objectMode: true,
+		transform(file, _, cb) {
+			if (!file.isBuffer()) {
+				return cb(new PluginError(PLUGIN_NAME, new TypeError('file should be a buffer')))
+			}
 
-            if (!options.entryPoints) {
-                const path = file.history[file.history.length - 1]
-                entries.push(path)
-            }
+			if (!options.entryPoints) {
+				const path = file.history[file.history.length - 1]
+				entries.push(path)
+			}
 
-            cb(null)
-        },
-        async flush(cb) {
-            const entry = options.entryPoints || entries
-            const params = {
-                ...options,
-                entryPoints: entry,
-                write: false
-            }
-            let data
+			cb(null)
+		},
+		async flush(cb) {
+			const entry = options.entryPoints || entries
+			const params = {
+				...options,
+				entryPoints: entry,
+				write: false
+			}
+			let data
 
-            try {
-                data = await build(params)
-            } catch(err) {
-                return cb(new PluginError(PLUGIN_NAME, err))
-            }
+			try {
+				data = await build(params)
+			} catch(err) {
+				return cb(new PluginError(PLUGIN_NAME, err))
+			}
 
-            const { outputFiles } = data
+			const { outputFiles } = data
 
-            outputFiles.forEach(it => {
-                const file = new Vinyl({
-                    path:     it.path,
-                    contents: Buffer.from(it.contents)
-                })
+			outputFiles.forEach(it => {
+				const file = new Vinyl({
+					path:     it.path,
+					contents: Buffer.from(it.contents)
+				})
 
-                this.push(file)
-            })
+				this.push(file)
+			})
 
-            cb(null)
-        }
-    })
+			cb(null)
+		}
+	})
 }
--- test/test.js
+++ test/test.js
@@ -7,60 +7,60 @@ const path = require('path')
 */
 
 test('prefer entryPoints option to src file', done => {
-    const stream = gulpEsbuild({
-        outdir: './',
-        entryPoints: [
-            resolve('a.js'),
-            resolve('b.js')
-        ]
-    })
+	const stream = gulpEsbuild({
+		outdir: './',
+		entryPoints: [
+			resolve('a.js'),
+			resolve('b.js')
+		]
+	})
 
-    execute(stream, [
+	execute(stream, [
 `console.log("a.js");
 `
-    ,
+	,
 `console.log("b.js");
 `
-    ]).then(done, done)
+	]).then(done, done)
 
-    stream.write(new File({
-        path:     resolve('a.js'),
-        contents: Buffer.from('')
-    }))
-    stream.end()
+	stream.write(new File({
+		path: resolve('a.js'),
+		contents: Buffer.from('')
+	}))
+	stream.end()
 })
 
 test('entry files number === output files number', done => {
-    const stream = gulpEsbuild({
-        outdir: './'
-    })
+	const stream = gulpEsbuild({
+		outdir: './'
+	})
 
-    execute(stream, [
+	execute(stream, [
 `console.log("a.js");
 `
-    ,
+	,
 `console.log("b.js");
 `
-    ]).then(done, done)
+	]).then(done, done)
 
-    stream.write(new File({
-        path:     resolve('a.js'),
-        contents: Buffer.from('')
-    }))
-    stream.write(new File({
-        path:     resolve('b.js'),
-        contents: Buffer.from('')
-    }))
-    stream.end()
+	stream.write(new File({
+		path: resolve('a.js'),
+		contents: Buffer.from('')
+	}))
+	stream.write(new File({
+		path: resolve('b.js'),
+		contents: Buffer.from('')
+	}))
+	stream.end()
 })
 
 test('bundle works', done => {
-    const stream = gulpEsbuild({
-        outfile: 'bundle.js',
-        bundle: true
-    })
+	const stream = gulpEsbuild({
+		outfile: 'bundle.js',
+		bundle: true
+	})
 
-    execute(stream, [
+	execute(stream, [
 `(() => {
   // test/fixtures/a.js
   console.log("a.js");
@@ -69,13 +69,13 @@ test('bundle works', done => {
   console.log("b.js");
 })();
 `
-    ]).then(done, done)
+	]).then(done, done)
 
-    stream.write(new File({
-        path:     resolve('c.js'),
-        contents: Buffer.from('')
-    }))
-    stream.end()
+	stream.write(new File({
+		path: resolve('c.js'),
+		contents: Buffer.from('')
+	}))
+	stream.end()
 })
 
 
@@ -84,53 +84,53 @@ test('bundle works', done => {
 */
 
 function resolve(name) {
-    return path.resolve(__dirname, 'fixtures', name)
+	return path.resolve(__dirname, 'fixtures', name)
 }
 
 function wrap(stream) {
-    return new Promise((resolve, reject) => {
-        const data = []
+	return new Promise((resolve, reject) => {
+		const data = []
 
-        stream.on('error', reject)
-        stream.on('end', () => {
-            resolve(data)
-        })
-        stream.on('data', chunk => {
-            data.push(chunk)
-        })
-    })
+		stream.on('error', reject)
+		stream.on('end', () => {
+			resolve(data)
+		})
+		stream.on('data', chunk => {
+			data.push(chunk)
+		})
+	})
 }
 
 function execute(stream, expected) {
-    return executeAll(stream, Array.isArray(expected) ? expected : [ expected ])
+	return executeAll(stream, Array.isArray(expected) ? expected : [ expected ])
 }
 
 function executeAll(stream, expected) {
-    return wrap(stream).then(files => {
-        const keys = Object.keys(expected)
+	return wrap(stream).then(files => {
+		const keys = Object.keys(expected)
 
-        // if length of array "expected" === 1 check number of output files
-        if (keys.length === 1) {
-            if (files.length !== 1) {
-                throw new Error(`expected 1 file, not ${ files.length }`)
-            }
-        }
+		// if length of array "expected" === 1 check number of output files
+		if (keys.length === 1) {
+			if (files.length !== 1) {
+				throw new Error(`expected 1 file, not ${ files.length }`)
+			}
+		}
 
-        // else check to number of array keys === number of output files
-        else if (keys.length !== files.length) {
-            throw new Error(`expected ${ keys.length } files, not ${ files.length }`)
-        }
+		// else check to number of array keys === number of output files
+		else if (keys.length !== files.length) {
+			throw new Error(`expected ${ keys.length } files, not ${ files.length }`)
+		}
 
-        let index = 0
+		let index = 0
 
-        for (const file of files) {
-            const expectedItem = expected[index]
-            const fileContents = file.contents.toString()
+		for (const file of files) {
+			const expectedItem = expected[index]
+			const fileContents = file.contents.toString()
 
-            expect(fileContents).toBe(expectedItem)
-            index++
-        }
+			expect(fileContents).toBe(expectedItem)
+			index++
+		}
 
-        return Promise.resolve()
-    })
+		return Promise.resolve()
+	})
 }
