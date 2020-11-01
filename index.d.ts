e6aeb59fbed5d97ca4d008842c001f3bf075b494
ym-project <ym-project@protonmail.com>
2020-11-01 15:32:57 +0800

    add "outbase" and "plugins" fields to the declaration file

--- index.d.ts
+++ index.d.ts
@@ -5,7 +5,7 @@ import * as stream from 'stream'
 type Format = 'iife' | 'cjs' | 'esm'
 type LogLevel = 'info' | 'warning' | 'error' | 'silent'
 type Platform = 'browser' | 'node'
-type Loader = 'js' | 'jsx' | 'ts' | 'tsx' | 'css' | 'json' | 'text' | 'base64' | 'file' | 'dataurl' | 'binary'
+type Loader = 'js' | 'jsx' | 'ts' | 'tsx' | 'css' | 'json' | 'text' | 'base64' | 'file' | 'dataurl' | 'binary' | 'default'
 type Charset = 'ascii' | 'utf8'
 
 interface CommonOptions {
@@ -39,6 +39,7 @@ interface BuildOptions extends CommonOptions {
 	outfile?: string
 	metafile?: string
 	outdir?: string
+	outbase?: string
 	platform?: Platform
 	external?: string[]
 	loader?: {
@@ -52,6 +53,77 @@ interface BuildOptions extends CommonOptions {
 	}
 	publicPath?: string
 	inject?: string[]
+	plugins: Plugin[]
+}
+
+interface Plugin {
+	name: string
+	setup: (build: PluginBuild) => void
+}
+
+interface PluginBuild {
+	onResolve(options: OnResolveOptions, callback: (args: OnResolveArgs) =>
+		(OnResolveResult | null | undefined | Promise<OnResolveResult | null | undefined>)): void
+	onLoad(options: OnLoadOptions, callback: (args: OnLoadArgs) =>
+		(OnLoadResult | null | undefined | Promise<OnLoadResult | null | undefined>)): void
+}
+
+interface OnResolveOptions {
+	filter: RegExp
+	namespace?: string
+}
+
+interface OnResolveArgs {
+	path: string
+	importer: string
+	namespace: string
+	resolveDir: string
+}
+
+interface OnResolveResult {
+	pluginName?: string
+
+	errors?: PartialMessage[]
+	warnings?: PartialMessage[]
+
+	path?: string
+	external?: boolean
+	namespace?: string
+}
+
+interface OnLoadOptions {
+	filter: RegExp
+	namespace?: string
+}
+
+interface OnLoadArgs {
+	path: string
+	namespace: string
+}
+
+interface OnLoadResult {
+	pluginName?: string
+
+	errors?: PartialMessage[]
+	warnings?: PartialMessage[]
+
+	contents?: string | Uint8Array
+	resolveDir?: string
+	loader?: Loader
+}
+
+interface PartialMessage {
+	text?: string
+	location?: Partial<Location> | null
+}
+
+interface Location {
+	file: string
+	namespace: string
+	line: number
+	column: number
+	length: number
+	lineText: string
 }
 
 declare const gulpEsbuild: (options: BuildOptions) => stream.Transform
