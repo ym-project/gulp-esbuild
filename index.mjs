cacde8532bad02c01dbdc16deee0d3771c58e684
ym-project <ym-project@protonmail.com>
2020-11-25 14:49:29 +0800

    add incremental building for watch mode

--- index.mjs
+++ index.mjs
@@ -55,3 +55,61 @@ export default function(options = {}) {
 		},
 	})
 }
+
+export function createGulpEsbuild() {
+	let promise
+
+	return function(options = {}) {
+		const entryPoints = []
+
+		return new Transform({
+			objectMode: true,
+			transform(file, _, cb) {
+				if (!file.isBuffer()) {
+					return cb(new PluginError(PLUGIN_NAME, new TypeError('file should be a buffer')))
+				}
+
+				entryPoints.push(file.path)
+				cb(null)
+			},
+			async flush(cb) {
+				const params = {
+					logLevel: 'silent',
+					...options,
+					entryPoints,
+					write: false,
+					incremental: true,
+				}
+
+				// set outdir by default
+				if (!options.outdir && !options.outfile) {
+					params.outdir = '.'
+				}
+
+				try {
+					// if it's the first build
+					if (!promise) {
+						promise = await build(params)
+					}
+					// if it's > 1 build
+					else {
+						promise = await promise.rebuild()
+					}
+				} catch(err) {
+					return cb(new PluginError(PLUGIN_NAME, err, {
+						showProperties: false,
+					}))
+				}
+
+				promise.outputFiles.forEach(file => {
+					this.push(new Vinyl({
+						path: file.path,
+						contents: Buffer.from(file.contents),
+					}))
+				})
+
+				cb(null)
+			},
+		})
+	}
+}
