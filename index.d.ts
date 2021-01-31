ef1b92496f2994f4316bcd2a599a74cc1ef2c169
ym-project <ym-project@protonmail.com>
2021-01-31 23:48:27 +0800

    exclude watch option from types

--- index.d.ts
+++ index.d.ts
@@ -3,7 +3,7 @@ import {BuildOptions} from 'esbuild'
 
 type Options = Omit<
 	BuildOptions,
-	'write' | 'incremental' | 'entryPoints' | 'stdin'
+	'write' | 'incremental' | 'entryPoints' | 'stdin' | 'watch'
 >
 
 type GulpEsbuild = (options: Options) => Transform
